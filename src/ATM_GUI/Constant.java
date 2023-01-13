f0039aa18fb21b60223ccb899354d4a6af23284b
Minh Le Cong <83758821+brianle169@users.noreply.github.com>
2023-01-13 16:40:13 +0700

    Update Constant.java

--- src/ATM_GUI/Constant.java
+++ src/ATM_GUI/Constant.java
@@ -7,5 +7,5 @@ public class Constant {
     protected static final int PANEL_HEIGHT = 550;
     protected static final int LI_WIDTH = 400;
     protected static final int LI_HEIGHT = 500;
-    protected static final String logInDataFile = "test_in.txt";
+    protected static final String logInDataFile = "Bank_LogInData.txt";
 }
