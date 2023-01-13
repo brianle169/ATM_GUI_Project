19eac801a4f169021dc68127d964bfc9fcbdfbf2
Minh Le Cong <83758821+brianle169@users.noreply.github.com>
2023-01-13 16:22:57 +0700

    Update Constant.java

--- Constant.java
+++ Constant.java
@@ -7,6 +7,5 @@ public class Constant {
     protected static final int PANEL_HEIGHT = 550;
     protected static final int LI_WIDTH = 400;
     protected static final int LI_HEIGHT = 500;
-    protected static final String logInDataFile = "test_in.txt";
-    protected static final String atmOutputFile = "test_out.txt";
+    protected static final String logInDataFile = ""; //Log in data file path
 }
